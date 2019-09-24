4c78c64e05533c603c14bebb933f930235c17068
miloujb <emilyjbailey@hotmail.co.uk>
2019-09-24 18:26:41 +0100

    start of de framework

---
+++ src/PageComponents/de.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const de = () => {
+  return (
+    <>
+      <div class="intro">
+        <h1>Emily Bailey</h1>
+        <h2>Entwicklerin | Linguistin | Northcoder</h2>
+      </div>
+      <div class="about"></div>
+      <div class="tech">
+        <h1>Technologiestack</h1>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default de;
