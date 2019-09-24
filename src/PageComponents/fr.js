bf2cb899471f62ba06e8ce3449d2de4df45ea122
miloujb <emilyjbailey@hotmail.co.uk>
2019-09-24 16:14:29 +0100

    fr framework finished

--- src/PageComponents/fr.js
+++ src/PageComponents/fr.js
@@ -9,7 +9,13 @@ const fr = () => {
       </div>
       <div class="about">
         <h1>À propos de moi</h1>
+        <p></p>
       </div>
+      <div class="tech">
+        <h1>Pile technologique</h1>
+      </div>
+      <div class="projects"></div>
+      <div class="contact"></div>
     </>
   );
 };
