bf4e4723861abfc566083594cb18fd5cb60ce5ab
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-05 12:28:01 +0100

    additional language pages deleted

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { Router } from "@reach/router";
 import "./App.css";
 import HomePage from "./PageComponents/HomePage";
-import English from "./PageComponents/en";
-import French from "./PageComponents/fr";
-import German from "./PageComponents/de";
-import Spanish from "./PageComponents/es";
 
 function App() {
   return (
@@ -14,10 +10,6 @@ function App() {
       <body>
         <Router>
           <HomePage path="/" />
-          <English path="/en" />
-          <French path="/fr" />
-          <German path="de" />
-          <Spanish path="es" />
         </Router>
       </body>
     </div>
