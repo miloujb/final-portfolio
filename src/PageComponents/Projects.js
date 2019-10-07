ae4142d5a79d0d2efd58322b864b8d6cad2c6a69
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-07 22:32:38 +0100

    second project added - aligning needed

--- src/PageComponents/Projects.js
+++ src/PageComponents/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import fencnews from "../images/fencnews.jpg";
+import reactorgrow from "../images/reactorgrow.jpg";
 
 const Projects = () => {
   return (
@@ -18,6 +19,34 @@ const Projects = () => {
             in JavaScript, using ReactJS, this fetches information from an API
             made up of a PostgreSQL database, using Express.
           </p>
+          <ul class="button-list">
+            <li>
+              <button>Github</button>
+            </li>
+            <li>
+              <button>Demo</button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>ReactorGrow - Mobile App</h2>
+          <img src={reactorgrow} alt="reactorGrow" class="reactorgrow"></img>
+          <div class="desctext">
+            <p>
+              A mobile app that is connected to an automated greenhouse
+              controlled by a Raspberry Pi and a Python back end. The app shows
+              the end user the most up-to-date data from the back end, and
+              allows the end user to override the signals sent to the
+              greenhouse. This lets the user water the plant or open the
+              greenhouse window as and when they require. This app was made with
+              React Native, hosted on Expo.
+            </p>
+            <ul class="button-list">
+              <li>
+                <button>Github</button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
