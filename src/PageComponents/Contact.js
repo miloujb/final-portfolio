6a181494b55a45536eeda4ba6cdc9a5f06a43eec
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-07 15:55:56 +0100

    links to linkedin and github

--- src/PageComponents/Contact.js
+++ src/PageComponents/Contact.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div class="contact">
       <h1>Contact Me</h1>
       <p>Email: emilybaileycodes@gmail.com</p>
-      <p>Github: www.github.com/miloujb</p>
+      <a href="https://github.com/miloujb" target="_blank">
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/emily-bailey-a9a647b7/"
+        target="_blank"
+      >
+        <FaLinkedinIn />
+      </a>
     </div>
   );
 };
